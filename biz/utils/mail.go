accbfd61e81d9c2db129c7dff9bb9ad0250f0075
lutasam <229644572@qq.com>
2022-11-26 20:29:56 +0800

    add mail feature

--- biz/utils/mail.go
+++ biz/utils/mail.go
@@ -1,1 +1,16 @@
 package utils
+
+import (
+	"gopkg.in/gomail.v2"
+)
+
+func SendMail(userName, host, mailTo, subject, body string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", m.FormatAddress(GetConfigString("mail.address"), GetConfigString("mail.username")))
+	m.SetHeader("To", mailTo)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+	d := gomail.NewDialer(host, 25, userName, GetConfigString("mail.auth"))
+	err := d.DialAndSend(m)
+	return err
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/onsi/gomega v1.24.1 // indirect
 	github.com/spf13/viper v1.14.0
 	golang.org/x/crypto v0.1.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gorm.io/driver/mysql v1.4.4
 	gorm.io/gorm v1.24.2
 )
